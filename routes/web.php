dfae63a56e3f46d77cfc983d3b21a7face37307f
BautistaZebaj <130525525+BautistaZebaj@users.noreply.github.com>
2024-02-21 10:15:59 +0800

    admin creation

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/app/admin', [AdminDashboard::class, 'index'])->name('app.admin.index');
