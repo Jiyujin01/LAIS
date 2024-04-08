1a67356317b74a2809c8e11e872f717473c9dcf9
Jiyujin01 <ccarcallas@sanagustinnhs.net>
2024-04-08 18:13:51 +0800

    addd

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,7 +4,7 @@
   <title>Web-Based ID Entry System</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="/favicons/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="vendor/adminlte/dist/img/icon.ico">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
